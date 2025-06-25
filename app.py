2da21863cd2a9b245ebd2b0f6c997c89a1582d0c
Ed Groell <email@edgroell.com>
2025-06-25 10:07:04 +0200

    new increments x4 - (1) added data structure in json format, (2) added index.html, (3) added style.css, and (4) added index function to render all blog posts

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+import os
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+DATA_PATH = os.path.join("data", "data.json")
+
+def fetch_data():
+    try:
+        with open(DATA_PATH, 'r') as f:
+            data = json.load(f)
+        return data
+    except FileNotFoundError:
+        print(f"Error: The file at {DATA_PATH} was not found.")
+        return []
+    except json.JSONDecodeError:
+        print("Error: The file contains invalid JSON.")
+        return []
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = fetch_data()
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
